234f83a43054480e2b6bb753d5dd538b42902ad1
abderrahmaneosnai <abderrahmaneosmani2@gmail.com>
2022-03-03 07:12:07 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
