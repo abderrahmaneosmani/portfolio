5c3b32808801c944a4f2d6fb2a0af36c55e2e604
abderrahmaneosnai <abderrahmaneosmani2@gmail.com>
2022-03-03 06:44:15 -0500

    update

--- pages/api/getPdf.ts
+++ pages/api/getPdf.ts
@@ -3,6 +3,7 @@ import { promisify } from "util";
 import fetch from "node-fetch";
 
 const pipeline = promisify(stream.pipeline);
+//firebase
 const url =
   "https://firebasestorage.googleapis.com/v0/b/portfolio-9a3ea.appspot.com/o/resume%2FOSMANI%20Abderrahmane.pdf?alt=media&token=c3ec7629-9797-47b2-9559-bfdbe5faa67e";
 
